e72a86caa628dc64efedcbddd971b99529e63d15
xglaxx <karla68santos@gmail.com>
2026-02-21 03:22:48 -0300

    Adicionando a função logsConsole

--- src/cli/test.js
+++ src/cli/test.js
@@ -2,15 +2,13 @@
 import Downloader from "./index.js";
 const pross = process.argv;
 const url = (pross[2] || "");
-const output = (pross[3] || "");
+const output = (pross[3] || "./tmp/"+Date.now());
 (async () => {
    if (!/https:\/\//.test(url)) throw url;
    
-   const job = Downloader({ url, output: (output || "./tmp/"+Date.now()) });
+   const job = Downloader({ url, output });
    job.on("progress", ({ percent, eta, speed }) => {
-      process.stdout.clearLine(0);
-      process.stdout.cursorTo(0);
-      process.stdout.write(`Progresso: ${percent}% | ETA: ${eta.toFixed(1)}s | Velocidade: ${(speed / 1024 / 1024).toFixed(2)} MB/s `);
+      job.logsConsole(`Progresso: ${percent}% | ETA: ${eta.toFixed(1)}s | Velocidade: ${(speed / 1024 / 1024).toFixed(2)} MB/s `);
    });
    job.on("completed", (data) => {
       console.warn("\nDownload Concluido:", data);
